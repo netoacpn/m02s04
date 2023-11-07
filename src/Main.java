df226514dc0e089ea3fa56c91975f21e6d99c6b0
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-11-06 22:40:36 -0300

    Adiciona Exercício 04

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.security.InvalidParameterException;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner(System.in);
+
+    boolean isIdadeValida = false;
+
+    int idade = 0;
+
+    while (!isIdadeValida){
+      System.out.print("Informe sua idade: ");
+      idade = scanner.nextInt();
+      try {
+        if (idade <= 0 || idade > 100){
+          throw new InvalidParameterException("Idade inválida!");
+        }
+        System.out.printf("Você tem %d anos de idade.", idade);
+        isIdadeValida = true;
+      } catch (InvalidParameterException e){
+        System.out.println(e.getMessage());
+      }
+    }
   }
 }
